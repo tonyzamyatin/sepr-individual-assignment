c2fcde750cc290f140da5ee9c61f7c7f32d53db6
tonyzamyatin <anton.zamyatin14@gmail.com>
2024-03-04 16:55:23 +0100

    template upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# SE PR Projekttemplate
+
+Bitte fügen Sie diese Datei, inklusive der beiliegenden `.gitlab-ci.yml` und den beiden Verzeichnissen `frontend` und `backend` zum Wurzelverzeichnis ihres Git-Repositories hinzu.
+Im folgenden befindet sich ein Template für die Stundenliste; bitte verwenden Sie es so, dass im GitLab-Projekt ihre Stundenliste als Tabelle sichtbar ist.
+
+Vergessen Sie nicht im Projekt ihren Namen und Matrikelnummer zu ersetzen.
+
+## Stundenliste
+
+**Name**: Anton Zamyatin\
+**Matrikelnummer**: 12223389
+
+
+| Datum | Startzeit | Dauer | Story-ID | Tätigkeit |
+|-------|-----------|-------|----------|-----------|
+| 04.03.|           |       |          |           |
+**Gesamtsumme der Zeit**: 0h 
---
+++ backend/src/main/java/at/ac/tuwien/sepr/assignment/individual/service/HorseValidator.java
@@ -0,0 +1,31 @@
+package at.ac.tuwien.sepr.assignment.individual.service;
+
+import at.ac.tuwien.sepr.assignment.individual.dto.HorseDetailDto;
+import at.ac.tuwien.sepr.assignment.individual.exception.ValidationException;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HorseValidator {
+  private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  public void validateForUpdate(HorseDetailDto horse) throws ValidationException {
+    LOG.trace("validateForUpdate({})", horse);
+    List<String> validationErrors = new ArrayList<>();
+
+    if (horse.id() == null) {
+      validationErrors.add("No ID given");
+    }
+
+    // TODO this is not complete…
+
+    if (!validationErrors.isEmpty()) {
+      throw new ValidationException("Validation of horse for update failed", validationErrors);
+    }
+  }
+
+}
